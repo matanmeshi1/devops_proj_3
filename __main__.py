0ec6f6cbe24fd3a376373d235401eec7a7876c84
matanmeshi1 <matanmeshi1@gmail.com>
2023-04-08 11:58:51 +0300

    set aws creds as env params

--- __main__.py
+++ __main__.py
@@ -1,4 +1,8 @@
 import boto3
+import os
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 
 def get_instance_name(instance, key='Name'):
@@ -15,7 +19,12 @@ def get_instance_name(instance, key='Name'):
 
 if __name__ == '__main__':
     print('Instances cleanup - Start!')
-    ec2 = boto3.resource('ec2', 'eu-west-1')
+    session = boto3.Session(
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    )
+    ec2 = session.resource('ec2', 'eu-west-1')
+
     instances = ec2.instances.filter(Filters=[{'Name': 'tag:k8s.io/role/master', 'Values': ['1']},
                                               {'Name': 'instance-state-code', 'Values': ['16']}])
     for instance in instances:
